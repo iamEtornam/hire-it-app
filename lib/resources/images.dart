867730d8e1c08b30c4bece4d84b53b8fe4727c4c
Etornam <sunumacbright@gmail.com>
2021-06-30 14:28:38 +0000

    generate image class based on images from asset

---
+++ lib/resources/images.dart
@@ -0,0 +1,12 @@
+part of 'resources.dart';
+
+class Images {
+  Images._();
+
+  static const String icons8Microsoft480 = 'assets/icons8-microsoft-480.png';
+  static const String icons8Google480 = 'assets/icons8-google-480.png';
+  static const String icons8Amazon480 = 'assets/icons8-amazon-480.png';
+  static const String avatar = 'assets/avatar.jpg';
+  static const String icons8AppleLogo512 = 'assets/icons8-apple-logo-512.png';
+  static const String icons8Netflix480 = 'assets/icons8-netflix-480.png';
+}
