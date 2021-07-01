5e3c7e6dec8cf6495a8142125c10ca5ab44d3431
Etornam <sunumacbright@gmail.com>
2021-07-01 10:39:34 +0000

    Update custom_logo.dart

--- lib/custom_logo.dart
+++ lib/custom_logo.dart
@@ -3,14 +3,21 @@ import 'package:hire_it/custom_colors.dart';
 
 class CustomLogo extends StatelessWidget {
   final double size;
-  const CustomLogo({ Key? key,required this.size}) : super(key: key);
+  const CustomLogo({Key? key, required this.size}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
       radius: size,
       backgroundColor: CustomColors.customBlueColor,
-      child: Center(child: Text('H',style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white,fontWeight: FontWeight.bold),)),
+      child: Center(
+          child: Text(
+        'H',
+        style: Theme.of(context)
+            .textTheme
+            .headline5!
+            .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+      )),
     );
   }
 }
